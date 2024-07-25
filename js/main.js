7d3efa2a8c62e22ddee05630e8f4e1b42ac59a08
Vasuema <133081276+Vasuema@users.noreply.github.com>
2024-07-24 23:05:17 -0600

    update styling, add toggle function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+// Step 1: select the element the user will click on to make this menu show/hide. In this case it's the toggle-icon and since we are grabbing it by it's classname we need to include the period 
+const clickButton = document.querySelector('.joystick');
+
+
+
+// Step 2: add a click event to that icon
+clickButton.addEventListener('click', () => {
+    // when that icon is clicked we are going to grab the nav element (or whatever is showing/hiding) and add or remove that special class we created in the CSS
+    document.querySelector('.first-level-nav').classList.toggle('show-nav');
+});
